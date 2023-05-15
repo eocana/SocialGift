508cc98d2c165303cdc64660ad4a2565ff8ce667
DEST-0 <75948576+DEST-0@users.noreply.github.com>
2023-05-15 11:33:12 +0200

    activity wishlist & main (2)

--- app/src/main/java/com/example/socialgift/view/MainActivity.java
+++ app/src/main/java/com/example/socialgift/view/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.socialgift.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.socialgift.R;
+import com.example.socialgift.model.User;
+import com.example.socialgift.model.Wishlist;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button user;
+    private Button wishlist;
+    private Button other;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
