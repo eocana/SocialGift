561b41b359db83c694e501e704771b3648319ca2
DEST-0 <75948576+DEST-0@users.noreply.github.com>
2023-05-24 21:19:18 +0200

    activity wishlist & main (4) // & User

--- app/src/main/java/com/example/socialgift/view/MainActivity.java
+++ app/src/main/java/com/example/socialgift/view/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.socialgift.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -20,7 +21,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        user = findViewById(R.id.btnUser);
+        wishlist = findViewById(R.id.btnWishlist);
+        other = findViewById(R.id.btnSocial);
 
+    }
 
+    public void goToUserActivity(User user) {
+        Intent intent = new Intent(this, UserActivity.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
+    }
+
+    public void goToWishlistActivity(Wishlist wishlist) {
+        Intent intent = new Intent(this, WishlistActivity.class);
+        intent.putExtra("wishlist", wishlist);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/socialgift/view/UserActivity.java
@@ -0,0 +1,19 @@
+package com.example.socialgift.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.socialgift.R;
+import com.example.socialgift.model.User;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+
+    }
+}
